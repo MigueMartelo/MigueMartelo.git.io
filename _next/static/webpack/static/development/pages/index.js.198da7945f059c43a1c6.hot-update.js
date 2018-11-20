1edc58d14ddec2285d1029c335857692194d1580
MigueMartelo <miguemartelo1031@gmail.com>
2018-11-20 15:38:47 -0500

    Primer commit

---
+++ _next/static/webpack/static/development/pages/index.js.198da7945f059c43a1c6.hot-update.js
@@ -0,0 +1,62 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Evento.js":
+/*!******************************!*\
+  !*** ./components/Evento.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/lidertecpc/Escritorio/JS/React/bitcoinapp/components/Evento.js";
+
+
+var Evento = function Evento(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Desde Eventos");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Evento);
+
+/***/ }),
+
+/***/ "./components/Eventos.js":
+/*!*******************************!*\
+  !*** ./components/Eventos.js ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Evento__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Evento */ "./components/Evento.js");
+var _jsxFileName = "/home/lidertecpc/Escritorio/JS/React/bitcoinapp/components/Eventos.js";
+
+
+
+var Eventos = function Eventos(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "Desde Eventos");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Eventos);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.198da7945f059c43a1c6.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.aa45b4de76d74e9c1512.hot-update.js
@@ -0,0 +1,73 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Noticia.js":
+/*!*******************************!*\
+  !*** ./components/Noticia.js ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/lidertecpc/Escritorio/JS/React/bitcoinapp/components/Noticia.js";
+
+
+var Noticia = function Noticia(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Desde Noticia");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Noticia);
+
+/***/ }),
+
+/***/ "./components/Noticias.js":
+/*!********************************!*\
+  !*** ./components/Noticias.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Noticia__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Noticia */ "./components/Noticia.js");
+var _jsxFileName = "/home/lidertecpc/Escritorio/JS/React/bitcoinapp/components/Noticias.js";
+
+
+
+var Noticias = function Noticias(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "row",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, props.noticias.map(function (noticia) {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Noticia__WEBPACK_IMPORTED_MODULE_1__["default"], {
+      key: noticia.id,
+      noticia: noticia,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 7
+      },
+      __self: this
+    });
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Noticias);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.aa45b4de76d74e9c1512.hot-update.js.map
